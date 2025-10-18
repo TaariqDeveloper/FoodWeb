98472112589dea0ccfe3baaf36d3a862b04b9709
TaariqDeveloper <TaariqDeveloper@gmail.com>
2025-10-18 16:14:29 +0300

    updated

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className='header'>
+            <div className='header-content'>
+                <h2>Order Your favourite food here</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos, alias dolore. Fuga nobis soluta eos voluptates blanditiis quia natus enim!</p>
+                <button>View Menu</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
